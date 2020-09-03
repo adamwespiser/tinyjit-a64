f8c74ddee206ce30661e62fea54265862c40d9c0
Adam Wespiser <adamwespiser@gmail.com>
2020-09-03 01:36:26 -0400

    some notes and stuff, keep chugging along

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ To find out more about ARM processors, aarch64 architecture, and the a64 instruc
 [Handy by Ian Connolly](https://github.com/minuteman3/Handy) An ARM emulator written in Haskell
 
 ## Helpful Resources
+[Index of Arm Intstructions](http://shell-storm.org/armv8-a/ISA_v85A_A64_xml_00bet8/xhtml/index.html) Enumerated list of all the ARM instructions with descriptions.    
 [Arm Architecture Reference Manual](https://developer.arm.com/documentation/ddi0487/fc/) This is was you'll need if you are going to actually encode asm instructions into binary.    
 [A64 Instruction Set Encoding](https://github.com/CAS-Atlantic/AArch64-Encoding/blob/master/binary%20encodding.pdf). Just the encodings.    
 [Guide to AARM64 by modexp](https://modexp.wordpress.com/2018/10/30/arm64-assembly/#registers). Good intro to asm.    
--- src/ASM.hs
+++ src/ASM.hs
@@ -1,47 +1,36 @@
+{-| Description: ASM representation of A64 ARM assembly
+
+A64 is a fixed width encoding, but the location of various operands varies by
+the type of the instruction.
+
+A "good representation" for this problem stradles between the following:
+ 1. representing the computation of the higher level language
+ 2. contains enough specific details to actually be computed on a ARM processor
+
+1. is pretty much the ASM type, and 2 is the encode function
+
+-}
 module ASM where
-
 import Data.Word
+import Data.Bits
 
-data Reg
- = X0 --arguments and retun values
- | X1
- | X2
- | X3
- | X4
- | X5
- | X6
- | X7
- | X8 -- temp values
- | X9
- | X10
- | X11
- | X12
- | X13
- | X14
- | X15
- | X16
- | X17
- | X18
- | X19 -- calle-saved registers
- | X20
- | X21
- | X22
- | X23
- | X24
- | X25
- | X26
- | X27
- | X28
- | X29  -- frame pointer
- | X30  -- stack pointer
+data Reg --arguments and retun values
+ = X0 | X1 | X2 | X3 | X4 | X5 | X6 | X7
+ -- temp values
+ | X8 | X9 | X10 | X11 | X12 | X13 | X14 | X15 | X16 | X17 | X18
+ -- calle-saved registers
+ | X19  | X20 | X21 | X22 | X23 | X24 | X25 | X26 | X27 | X28
+ -- frame pointer
+ | X29
+ -- stack pointer
+ | X30
  deriving (Eq, Show, Enum)
 
+-- Not sure if this is a good idea, in ARM Imm and Reg values are
+-- not exchangable like they are in x86
+data Val = I Word8 | R Reg deriving (Eq, Show)
 
-data Val
- = I Word8
- | R Reg
- deriving (Eq, Show)
-
+-- | Is the Instr for 64 or 32 bit mode?
 data SubHeadingField
   = Variant32Bit
   | Variant64Bit
@@ -83,9 +72,24 @@ data Instr
   = STR Reg Reg Offset
   | LDR Reg Reg Offset
   | MOV Reg Reg
-  | MOVK MovK Word16 Reg
-  | RET
-  | SVC Word16
+  | MOVK MovK Word16 Reg --move/shift
+  | RET (Maybe Reg)
+  | SVC Word16 --supervisor/system call
+  | SYS --system call
+  | Br -- branch
+  | ADD Reg Reg
 
+zero32 :: Word32
+zero32 = fromInteger 0
 
+-- | Lets just try something simple to get started
+--   then once we get an idea for the pattern, we can use one
+--   of the few known patterns here.
+--   Note: Instruction size is 32bit for 32/64bit reg size
+encode :: Instr -> Word32
+encode (RET Nothing) = 0xD65F0000 -- we can probably replace this "magic  number"
+                                  -- with a smaller magic number, on per instruction type
+encode (RET (Just reg)) = 0xD65F0000
+  .|. ((fromIntegral @Int @Word32 . fromEnum $ reg ) `shiftL` 5)
+encode _ = zero32
 
